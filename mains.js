96373ac48da850eaa79151f9270e0d701883c136
GUSTAVO TEIXEIRA <25203634@CFP127ALUNO.LOCAL>
2026-02-02 11:24:22 -0300

    sistema de tradução

--- mains.js
+++ mains.js
@@ -2,9 +2,23 @@
 
 const ButtonTrocaCor = document.getElementById('trocar-cor')
 
+
+
 function trocaCor(){
-    const cor = document.getElementById('cor').value 
-    document.documentElement.style.setProperty('--cor-bg', cor)
+    const cor = document.getElementById('cor', ).value
+
+    const cores = {
+        vermelho : 'red',
+        azul : 'blue',
+        laranja : 'orange',
+        vermelho : 'red'
+    }
+
+if(cores[cor]){
+    document.documentElement.style.setProperty('--cor-bg', cores[cor])
+}else{alert("erro")}
+   
+    
 
 }
 ButtonTrocaCor.addEventListener('click', trocaCor)
